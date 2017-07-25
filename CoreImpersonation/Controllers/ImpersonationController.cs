5c155c879ee84cdf20bf4caa4ceb37200ca5c322
Max Vasilyev <trailmax1@gmail.com>
2017-07-26 00:55:01 +0100

    initial commit

---
+++ CoreImpersonation/Controllers/ImpersonationController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using CoreImpersonation.Models;
+using CoreImpersonation.Models.AccountViewModels;
+using CoreImpersonation.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+
+namespace CoreImpersonation.Controllers
+{
+    public class ImpersonationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IdentityCookieOptions cookieOptions;
+
+        public ImpersonationController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IOptions<IdentityCookieOptions> identityCookieOptions)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            cookieOptions = identityCookieOptions.Value;
+        }
+
+
+        [Authorize]
+        public ViewResult ListOfUsers()
+        {
+            var users = _userManager.Users.ToList();
+
+            return View(users);
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> ImpersonateUser(String userId)
+        {
+            var currentUserId = User.GetUserId();
+
+            var impersonatedUser = await _userManager.FindByIdAsync(userId);
+
+            var userPrincipal = await _signInManager.CreateUserPrincipalAsync(impersonatedUser);
+
+            userPrincipal.Identities.First().AddClaim(new Claim("OriginalUserId", currentUserId));
+            userPrincipal.Identities.First().AddClaim(new Claim("IsImpersonating", "true"));
+            
+            // sign out the current user
+            await _signInManager.SignOutAsync();
+
+            await HttpContext.Authentication.SignInAsync(cookieOptions.ApplicationCookieAuthenticationScheme, userPrincipal);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> StopImpersonation()
+        {
+            if (!User.IsImpersonating())
+            {
+                throw new Exception("You are not impersonating now. Can't stop impersonation");
+            }
+
+            var originalUserId = User.FindFirst("OriginalUserId").Value;
+
+            var originalUser = await _userManager.FindByIdAsync(originalUserId);
+
+            await _signInManager.SignOutAsync();
+
+            await _signInManager.SignInAsync(originalUser, isPersistent: true);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CoreImpersonation/Models/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace CoreImpersonation.Models
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        //https://stackoverflow.com/a/35577673/809357
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentNullException(nameof(principal));
+            }
+            var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+
+            return claim?.Value;
+        }
+
+        public static bool IsImpersonating(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentNullException(nameof(principal));
+            }
+
+            var isImpersonating = principal.HasClaim("IsImpersonating", "true");
+
+            return isImpersonating;
+        }
+    }
+}
